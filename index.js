7c54de8502ef289635d7932c065caf76e5c20b2e
Pauloricardo1808 <pr243140@gmail.com>
2025-11-18 13:45:54 -0300

    fix: correcao mensagem API

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors({ origin: 'https://projeto-cicd-frontend.vercel.app' }));
 
 app.get('/', (req, res) => {
   res.json({
-    mensagem: "API FUNCIONANDO COM SUCESSO! 🚀",
+    mensagem: "API FUNCIONANDO COM SUCESSO!",
     versao_atual: version
   });
 });
