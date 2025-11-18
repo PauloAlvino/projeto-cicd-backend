29b8173052692c9bf347862e25e8e13461dbbf41
Pauloricardo1808 <pr243140@gmail.com>
2025-11-18 13:15:25 -0300

    fix: mudança da logica base

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const os = require('os');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const { version } = require('./package.json'); 
+
+const { version } = require('./package.json');
 
 app.use(cors({ origin: 'https://projeto-cicd-front.vercel.app' }));
 
 app.get('/', (req, res) => {
-  const uptime = process.uptime();
-  const uptimeFormatado = new Date(uptime * 1000).toISOString().substr(11, 8);
-
   res.json({
-    status: "OPERACIONAL 🟢",
-    mensagem: "API de Monitoramento CI/CD",
-    versao: version,
-    sistema: {
-      os: os.type(),
-      hostname: os.hostname(),
-      memoriaLivre: `${(os.freemem() / 1024 / 1024).toFixed(2)} MB`,
-      tempoOnline: uptimeFormatado
-    },
-    timestamp: new Date().toLocaleString('pt-BR')
+    mensagem: "API FUNCIONANDO COM SUCESSO! 🚀",
+    versao_atual: version
   });
 });
 
